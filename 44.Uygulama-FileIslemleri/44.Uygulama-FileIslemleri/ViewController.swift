b055aa4bd366dcfedd3bb466e99eb0363bbc07c1
Yasin Özmen <yasinozmeen3@gmail.com>
2022-07-16 02:14:27 +0300

    FileManeger ile veri yazmayı okumayı ve silmeyi öğrendim

--- 44.Uygulama-FileIslemleri/44.Uygulama-FileIslemleri/ViewController.swift
+++ 44.Uygulama-FileIslemleri/44.Uygulama-FileIslemleri/ViewController.swift
@@ -8,12 +8,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var resimTutucu: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func yaz(_ sender: Any) {
+        let mesaj = textField.text!
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            
+            let dosyaYolu = dir.appendingPathComponent("dosyam.txt")
+            
+            do {
+                try mesaj.write(to: dosyaYolu, atomically: false, encoding: String.Encoding.utf8)
+            }
+            catch{ print("hataOldu")}
+        }
+    }
+    @IBAction func oku(_ sender: Any) {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            
+            let dosyaYolu = dir.appendingPathComponent("dosyam.txt")
+            
+            do {
+                textField.text =  try String(contentsOf: dosyaYolu, encoding: .utf8)
+            }
+            catch { print("2HataOldu")}
+        }
+    }
+    @IBAction func sil(_ sender: Any) {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            
+            let dosyaYolu = dir.appendingPathComponent("dosyam.txt")
+            
+            if FileManager.default.fileExists(atPath: dosyaYolu.path){
+                
+                do{
+                    try FileManager.default.removeItem(at: dosyaYolu)
+                    textField.text = ""
+                }
+                catch{print("3HataOldu") }
+            }
+        }
+    }
+    @IBAction func resmiKaydet(_ sender: Any) {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            let dosyaYolu = dir.appendingPathComponent("batman.jpg")
+            
+            let resmim = UIImage(named: "batman")
+            
+            do{
+                try resmim!.pngData()?.write(to: dosyaYolu)
+            }
+            catch{ }
+        }
+    }
+    @IBAction func resmiGöster(_ sender: Any) {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            let dosyaYolu = dir.appendingPathComponent("batman.jpg")
+            
+            self.resimTutucu.image = UIImage(contentsOfFile: dosyaYolu.path)
+        }
+    }
+    @IBAction func resmiSil(_ sender: Any) {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            
+            let dosyaYolu = dir.appendingPathComponent("batman.jpg")
+            
+            if FileManager.default.fileExists(atPath: dosyaYolu.path){
+                do{
+                    try FileManager.default.removeItem(at: dosyaYolu)
+                }
+                catch { }
+            }
+        }
+    }
+    
 
 }
 
