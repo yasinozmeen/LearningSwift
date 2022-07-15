1e13dd114f9cd56a1379a7264e54f156d5394406
Yasin Özmen <54503469+yasinozmeen@users.noreply.github.com>
2022-07-15 23:15:29 +0300

    fileİslemleri dosyası yüklendi-Güncelleme gelicek

---
+++ 44.Uygulama-FileIslemleri/44.Uygulama-FileIslemleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  44.Uygulama-FileIslemleri
+//
+//  Created by Kadir Yasin Özmen on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
