d34d0c20212ae7e745d272bbbfbb82339e22d26b
Yasin Özmen <yasinozmeen3@gmail.com>
2022-07-20 20:12:50 +0300

    CoreDataEgzersiz Başlangç
    GüncellemeGelecek..

---
+++ CoreDataEgzersiz/CoreDataEgzersiz/DetayVC.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class DetayVC: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+    @IBOutlet var label4: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
---
+++ CoreDataEgzersiz/CoreDataEgzersiz/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var anaSayfaLabel: UILabel!
+    @IBOutlet var textField1: UITextField!
+    @IBOutlet var textField2: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func button(_ sender: Any) {
+    }
+    
+
+}
+
