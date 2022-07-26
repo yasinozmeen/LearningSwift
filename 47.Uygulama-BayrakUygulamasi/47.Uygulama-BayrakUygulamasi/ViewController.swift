03b1784a2baebcf3c5ea657b438289f722bc2fa8
Yasin Özmen <yasinozmeen3@gmail.com>
2022-07-26 17:01:06 +0300

    SQLite kullanarak hangi bayrak hangi ülkenin tahmin ettiğimiz bir oyun yapıyorum.
     Taslak tasarımı bitti.

---
+++ 47.Uygulama-BayrakUygulamasi/47.Uygulama-BayrakUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  47.Uygulama-BayrakUygulamasi
+//
+//  Created by Kadir Yasin Özmen on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
