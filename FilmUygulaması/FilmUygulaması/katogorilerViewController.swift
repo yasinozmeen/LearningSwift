8cbc0d390a66e9b1c976d606de7d4d82b69e2897
Yasin Özmen <yasinozmeen3@gmail.com>
2022-07-31 12:42:29 +0300

    sql dosyaları ve viewControllerlar eklendi

---
+++ FilmUygulaması/FilmUygulaması/katogorilerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  katogorilerViewController.swift
+//  FilmUygulaması
+//
+//  Created by Kadir Yasin Özmen on 31.07.2022.
+//
+
+import UIKit
+
+class katogorilerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
