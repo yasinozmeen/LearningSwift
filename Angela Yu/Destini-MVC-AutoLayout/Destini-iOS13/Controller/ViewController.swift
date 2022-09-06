b8fbc9d536a49a3bbb1317d685ba2162a02fee39
Yasin Özmen <yasinozmeen3@gmail.com>
2022-09-06 22:50:27 +0300

    final commit

---
+++ Angela Yu/Destini-MVC-AutoLayout/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUi()
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(sender.currentTitle!)
+        
+        updateUi()
+    }
+    
+    func updateUi(){
+        let story = storyBrain.storys[storyBrain.storyNumber]
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    
+}
+
---
+++ Angela Yu/Destini-MVC-AutoLayout/Destini-iOS13/Model/Story.swift
@@ -0,0 +1,27 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Story{
+    var title: String?
+    var choice1: String?
+    var choice2: String?
+    var choice1Destination:Int?
+    var choice2Destination:Int?
+    
+    init(title: String,choice1: String,choice1Destination:Int,choice2: String,choice2Destination:Int){
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1Destination = choice1Destination
+        self.choice2Destination = choice2Destination
+    }
+   
+
+}
