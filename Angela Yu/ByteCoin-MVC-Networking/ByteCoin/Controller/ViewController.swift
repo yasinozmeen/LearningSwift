f516951e706dcb9873a4ea4546b3ad13b2c6388d
Yasin Özmen <yasinozmeen3@gmail.com>
2022-09-13 19:41:25 +0300

    preparing networking

--- Angela Yu/ByteCoin-MVC-Networking/ByteCoin/Controller/ViewController.swift
+++ Angela Yu/ByteCoin-MVC-Networking/ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+        
+    @IBOutlet var currencvLabel: UILabel!
+    @IBOutlet var bitcoinLabel: UILabel!
+    @IBOutlet var currencyPicker: UIPickerView!
+    
+    let coinManeger = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
+        coinManeger.fetchPrice(url: coinManeger.baseURL)
     }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManeger.currencyArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManeger.currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCoin = coinManeger.currencyArray[row]
+        coinManeger.getCoinPrice(for: selectedCoin)
+    }
 }
 
--- Angela Yu/ByteCoin-MVC-Networking/ByteCoin/Model/CoinManager.swift
+++ Angela Yu/ByteCoin-MVC-Networking/ByteCoin/Model/CoinManager.swift
@@ -10,10 +10,34 @@ import Foundation
 
 struct CoinManager {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=2F9F5589-3D63-4A95-BD81-DBDEA83B8A13"
+    
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for currency:String){
+        print(currency)
+    }
+    
+    func fetchPrice(url:String){
+        
+        print("1")
+        if let url = URL(string: url){
+            print("2")
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                print("3")
+                if error != nil{
+                    print(error)
+                }else{
+                    if let safeData = data{
+                        print(String(data: safeData, encoding: .utf8)!)
+                    }
+                    
+                }
+            }
+            task.resume()
+        }
+        
+    }
 }
