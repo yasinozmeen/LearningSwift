28b3de4382db6aea69909405aa4b288e9cae2406
Yasin Özmen <yasinozmeen3@gmail.com>
2022-09-13 14:26:17 +0300

    final commit

--- Angela Yu/Clima-iOS13-MVC-Networking/Clima/Controller/WeatherViewController.swift
+++ Angela Yu/Clima-iOS13-MVC-Networking/Clima/Controller/WeatherViewController.swift
@@ -7,21 +7,54 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet var searchTextField: UITextField!
     
     var weatherManeger = WeatherManeger()
+    let locationManeger = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManeger.delegate = self
+        locationManeger.requestWhenInUseAuthorization()
+        locationManeger.requestLocation()
+        
         weatherManeger.delegate = self
         searchTextField.delegate = self
     }
+    @IBAction func currentLocationPressed(_ sender: Any) {
+        locationManeger.requestLocation()
+    }
+    
+}
+//MARK: - CLLocationManager
+extension WeatherViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error.localizedDescription)")
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let locations = locations.last{
+            locationManeger.stopUpdatingLocation()
+            let lat = locations.coordinate.latitude
+            let lon = locations.coordinate.longitude
+            print(lat,lon)
+            weatherManeger.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+}
+
+//MARK: - TextFieldDelegate
+extension WeatherViewController: UITextFieldDelegate{
     @IBAction func searchPressed(_ sender: Any) {
         searchTextField.endEditing(true)
         print(searchTextField.text!)
@@ -44,6 +77,9 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
             weatherManeger.fetchWeather(cityName: city)
         }
     }
+}
+//MARK: - WeatherMAnagerDelegete
+extension WeatherViewController: WeatherManagerDelegate{
     func didUpdateUi(weather: WeatherModel) {
         DispatchQueue.main.async {
             self.temperatureLabel.text = weather.temperatureString
